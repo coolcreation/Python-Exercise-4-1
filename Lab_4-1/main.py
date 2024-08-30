dbbac5cab18a3df032f81eb707307e40f2f52742
coolcreation <73644110+coolcreation@users.noreply.github.com>
2024-08-30 12:15:50 -0600

    Add project folder

---
+++ Lab_4-1/main.py
@@ -0,0 +1,46 @@
+# Jeff Bohn
+# SWDV-210 Lab 4 : Future Value Calculator
+# 8/30/2024
+
+import validation as isValid
+
+def calculate_future_userNumber(monthly_investment, yearly_interest_rate, years):
+    # convert yearly values to monthly values
+    monthly_interest_rate = yearly_interest_rate / 12 / 100
+    months = years * 12
+
+    # calculate future value
+    future_value = 0.0
+    for i in range(months):
+        future_value += monthly_investment
+        monthly_interest = future_value * monthly_interest_rate
+        future_value += monthly_interest
+
+    return future_value
+
+
+def main():
+    choice = "y"
+    while choice.lower() == "y":
+
+        monthly_investment = isValid.getFloat("\nEnter monthly investment:\t", low=0, high=1000)
+
+        yearly_interest_rate = isValid.getFloat("\nEnter yearly interest rate:\t", low=0, high=15)
+
+        years = isValid.getInt("\nEnter number of years:\t\t", low=0, high=50)
+
+        # get and display future userNumber
+        future_userNumber = calculate_future_userNumber(monthly_investment, yearly_interest_rate, years)
+
+        print(f"Future value:\t\t\t ${round(future_userNumber, 2)} ")
+        print()
+
+        # see if the user wants to continue
+        choice = input("Continue? (y/n): ")
+        print()
+
+    print("Bye!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Lab_4-1/validation.py
@@ -0,0 +1,43 @@
+# Jeff Bohn
+# SWDV-210 Lab 4 : Future Value Calculator
+# 8/30/2024
+
+# Learning docstrings in Python
+# 1 - Does it give a high level overview of what you are trying to accomplish?
+# 2 - Does it have full coverage of arguments passed in?
+# 3 - Does it cover the method or class return type and return value?
+# 4 - Understand how my team operates/communicates and note accordingly
+# 5 - Type hinting can be useful in loose languages like Python
+
+
+
+def getFloat(prompt, low, high):
+    
+    """
+    getFloat() validates user input for the specific number range \n
+    args: float(prompt), int(low), int(high) \n
+    return: float(userNumber)
+    """
+    # run indefinitely (keep asking user) until return statement
+    while True:
+        userNumber = float(input(prompt))
+        if (low <= userNumber and userNumber <= high):
+            return userNumber
+        else:
+            print(f"Enter a number greater than {low} and less than or equal to {high}")
+
+
+def getInt(prompt, low, high):
+    
+    """
+    getInt() validates user input for the specific number range \n
+    args: int(prompt), int(low), int(high) \n
+    return: int(userNumber)
+    """
+    
+    while True:
+        userNumber = int(input(prompt))
+        if (low <= userNumber and userNumber <= high):
+            return userNumber
+        else:
+            print(f"Enter a number greater than {low} and less than or equal to {high}")
